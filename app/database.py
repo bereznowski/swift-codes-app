72d6df6d51cff1fee791d604701f50d2e330f255
bereznowski <79934148+bereznowski@users.noreply.github.com>
2025-04-29 14:47:27 +0200

    add database

---
+++ app/database.py
@@ -0,0 +1,13 @@
+"""This module is responsible for creating database and tables."""
+
+from sqlmodel import SQLModel, create_engine
+
+SQLITE_FILE_NAME = "database.db"
+SQLITE_URL = f"sqlite:///{SQLITE_FILE_NAME}"
+
+engine = create_engine(SQLITE_URL)
+
+
+def create_db_and_tables():
+    """Creates database and tables."""
+    SQLModel.metadata.create_all(engine)
