d62071da6d736a92cc9d4c23c39a51aeea1fe9a0
bereznowski <79934148+bereznowski@users.noreply.github.com>
2025-04-29 11:39:03 +0200

    refine extracting data from excel

--- app/data_processing.py
+++ app/data_processing.py
@@ -1,50 +1,69 @@
-"""
-This module is responsible for extracting required information from the Excel Spreadsheet.
-"""
+"""This module is responsible for extracting required information from the Excel Spreadsheet."""
 
-import os
-import numpy as np
 import pandas as pd
 
 
 def extract_banks_data(file_path: str):
+    """Extracts banks data from the Excel file.
+
+    Parameters
+    ----------
+    file_path : str
+        The path of the Excel file.
+
+    Returns
+    -------
+    dict
+        Dictionary of values to be used during banks' table model creation.
+    """
     columns_to_use = ["SWIFT CODE", "NAME", "ADDRESS", "COUNTRY ISO2 CODE"]
     columns_renaming_dict = {
         "SWIFT CODE": "swift_code",
         "NAME": "name",
         "ADDRESS": "address",
-        "COUNTRY ISO2 CODE": "iso2",
+        "COUNTRY ISO2 CODE": "country_iso2",
     }
 
-    df_banks = (
-        pd.read_excel(file_path, usecols=columns_to_use)
-        .rename(columns=columns_renaming_dict)
-        .replace({np.nan: None})
+    df = pd.read_excel(file_path, usecols=columns_to_use).rename(
+        columns=columns_renaming_dict
     )
 
-    df_banks["is_headquarter"] = df_banks["swift_code"].str.endswith("XXX")
-    df_banks["base_code"] = df_banks["swift_code"].str[:8]
-    df_banks.loc[~df_banks["is_headquarter"], "hq_swift_code"] = df_banks[
-        "base_code"
-    ] + str("XXX")
-    df_banks = df_banks.sort_values(by="is_headquarter", ascending=False)
+    df["is_headquarter"] = df["swift_code"].str.endswith("XXX")
+    df["potential_hq"] = df["swift_code"].str[:8] + str(
+        "XXX"
+    )  # these are potential headquarters
+    # branches without a headquarter exist (e.g., ALBPPLP1BMW)
+    # checks are performed during DB insertion
 
-    print(df_banks.head())
-    print(df_banks.tail())
+    return df.sort_values(  # headquarters need to be inserted first for the referential integrity
+        by="is_headquarter", ascending=False
+    ).to_dict(
+        "records"
+    )
 
 
 def extract_countries_data(file_path: str):
+    """Extracts countries data from the Excel file.
+
+    Parameters
+    ----------
+    file_path : str
+        The path of the Excel file.
+
+    Returns
+    -------
+    dict
+        Dictionary of values to be used during countries' table model creation.
+    """
     columns_to_use = ["COUNTRY ISO2 CODE", "COUNTRY NAME"]
     columns_renaming_dict = {
-        "COUNTRY ISO2 CODE": "country_iso2",
-        "COUNTRY NAME": "country_name",
+        "COUNTRY ISO2 CODE": "iso2",
+        "COUNTRY NAME": "name",
     }
 
     return (
         pd.read_excel(file_path, usecols=columns_to_use)
-        .drop_duplicates()
+        .drop_duplicates()  # there can be many banks from the same country
         .rename(columns=columns_renaming_dict)
         .to_dict("records")
     )
-
-extract_banks_data("../data/swift_codes.xlsx")
